eb9c5bca902f198d20cace8881c8c1bd7a65dc3c
irinaugrak <irina.ugrak@gmail.com>
2022-10-31 14:54:57 +0300

    fibonacci function

---
+++ homework_9/script.js
@@ -0,0 +1,22 @@
+function makeFibonacciFunction() {
+    let a = 0;
+    let b = 1;
+    console.log(a);
+    console.log(b);
+    return () => {
+        let c = a + b;
+        console.log(c);
+        a = b;
+        b = c;
+    }
+};
+
+const fibonacci = makeFibonacciFunction();
+
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
